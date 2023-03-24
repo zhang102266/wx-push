7a13b9baafe55ea88650e47c53f6ee2abd6b87d9
zhang102266 <128821095+zhang102266@users.noreply.github.com>
2023-03-24 22:14:01 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
+  appid: "wxb0244ae5ce0c4d55",
   appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  template_id: "ubz7d3WxcYejvLbN5CPdjlkVU40IprUDOm7HWsrD8nQ",
+  touser: "oG6MY6F2pd1p_CQBwoLW2zqjkoJo",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "南京",
+  first_date: "2023-03-22",
 };
